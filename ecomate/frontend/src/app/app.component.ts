52026dccb25a149271281ee9ad0591a25e670fac
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2025-07-03 23:38:36 +0500

    Updated App Component

--- ecomate/frontend/src/app/app.component.ts
+++ ecomate/frontend/src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet,CommonModule, FormsModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isLoggedIn = false;
+
+  ngOnInit() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus() {
+    // Check if token exists in localStorage
+    const token = localStorage.getItem('auth_token');
+    this.isLoggedIn = !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('user');
+    this.isLoggedIn = false;
+    window.location.href = '/login'; // reload to force route update
+  }
 }
